9cbf9ad94cefc2a635e0df7568b562c2a50498b3
Nadyne Madureira <madureira.nadyne@gmail.com>
2022-07-15 19:16:01 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.devsuperior.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -20,8 +21,11 @@ public class SaleController {
 	 *  controller chama service, service chama o repository */ 
 	
 	@GetMapping
-	public List<Sale> findSales(){
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate,
+			Pageable pageable){
+		return service.findSales(minDate, maxDate, pageable);
 	}
 	/* método que disponibiliza a lista de vendas para o frontend. 
 	 * O GetMapping serve para o metodo responder via web por meio do protocolo http */
