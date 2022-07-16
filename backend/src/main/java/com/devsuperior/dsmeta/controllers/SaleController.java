68e4ef837ae9ec018ccd5905b61df5bdf1197188
Nadyne Madureira <madureira.nadyne@gmail.com>
2022-07-16 20:05:58 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -20,6 +22,9 @@ public class SaleController {
 	/* variacao do padrao de tres camadas: controller, service, repository;
 	 *  controller chama service, service chama o repository */ 
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping
 	public Page<Sale> findSales(
 			@RequestParam(value="minDate", defaultValue="") String minDate,
@@ -29,5 +34,10 @@ public class SaleController {
 	}
 	/* método que disponibiliza a lista de vendas para o frontend. 
 	 * O GetMapping serve para o metodo responder via web por meio do protocolo http */
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
 
 }
